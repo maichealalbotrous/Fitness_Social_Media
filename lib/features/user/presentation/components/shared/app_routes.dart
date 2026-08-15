bef74683a36b777844e543eb5210ed1ed56991ed
maichealalbotrous <1.54495661e+08+maichealalbotrous@users.noreply.github.com>
2026-08-15 16:55:43 +0000

    fix(routing): avoid root route conflict with auth gate

--- lib/features/user/presentation/components/shared/app_routes.dart
+++ lib/features/user/presentation/components/shared/app_routes.dart
@@ -2,7 +2,7 @@ class AppRoutes {
   const AppRoutes._();
 
   static const auth = '/auth';
-  static const feed = '/';
+  static const feed = '/feed';
   static const workouts = '/workouts';
   static const runs = '/runs';
   static const profile = '/profile';
